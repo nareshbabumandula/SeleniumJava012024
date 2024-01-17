b34c1eccdc57d0dca8d008380a39158d6fb3d111
snehabompelly <b.sneha157@gmail.com>
2024-01-17 18:34:45 +0530

    17-01-2024:sneha - Added Sampleprogram.java file

--- SeleniumJavaBatch/src/program1/Sampleprogram.java
+++ SeleniumJavaBatch/src/program1/Sampleprogram.java
@@ -6,10 +6,12 @@ public class Sampleprogram {
 		byte b1=5;
 		short s1=20;
 		int f1=100;
+		long l1=999l;
 		
 		System.out.println(b1);
 		System.out.println(s1);
 		System.out.println(f1);
+		System.out.println(l1);
 
 	}
 

c94ff27d1c6f6f01291742d3db7ad5aa15b7b6bb
nareshbabumandula <naresh223@gmail.com>
2024-01-25 08:16:46 +0530

    Upcasting and downcasting

--- SeleniumJavaBatch/src/com/oops/Car.java
+++ SeleniumJavaBatch/src/com/oops/Car.java
@@ -1,5 +1,20 @@
 package com.oops;
 
 public class Car {
+	
+	double price=1000000.00; 
+	
+	void drive() {
+		System.out.println("Driving in super class..!");
+	}
+	
+	public Car() {
+		System.out.println("Car class constructor..!");
+	}
+	
+	public static void main(String[] args) {
+		Car car = new Car();
+		System.out.println("Main method 1");
+	}
 
 }
--- SeleniumJavaBatch/src/com/oops/Fortuner.java
+++ SeleniumJavaBatch/src/com/oops/Fortuner.java
@@ -1,28 +1,31 @@
 package com.oops;
 
-public class Fortuner {
+public class Fortuner extends Car{
+
+	double price=2000000.00; 
 	
 	// Method overloading/Compile time Polymorphism -- Having same methods in a class with different signature
-	void drive() {
-		System.out.println("Driving..!");
+
+	void drive() { 
+		System.out.println("Driving..!"); 
 	}
-	
+
 	void drive(String mode) {
 		System.out.println("Driving in " + mode + " mode");
 	}	
-	
+
 	void drive(String mode, int speed) {
 		System.out.println("Driving in " + mode + " mode with speed " + speed + " kmph");
 	}
-	
+
 	void drive(String mode, int speed, double mileage) {
 		System.out.println("Driving in " + mode + " mode with speed " + speed + " kmph with mileage of " + mileage + " kmpl");
 	}
-	
+
 	public Fortuner() {
 		System.out.println("This is a no argument constructor for Fortuner class");
 	}	
-	
+
 	public Fortuner(String model) {
 		System.out.println("This is a parameterized constructor for Fortuner class with model " + model);
 	}
@@ -35,9 +38,10 @@ public class Fortuner {
 		fort.drive("normal");
 		fort.drive("sports", 100);
 		fort.drive("eco", 80, 15);
-		
+		System.out.println(fort.price);
+
 		Fortuner fortuner = new Fortuner("4X4 Automatic");
-		
+
 	}
 
 }

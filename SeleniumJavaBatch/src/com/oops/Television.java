df130edf232794547c3537858c7c4a5c85b8510b
nareshbabumandula <naresh223@gmail.com>
2024-01-31 07:09:00 +0530

    Interface in Java

--- SeleniumJavaBatch/src/com/oops/Television.java
+++ SeleniumJavaBatch/src/com/oops/Television.java
@@ -1,5 +1,13 @@
 package com.oops;
-
+/**
+ * 1. Abstraction : Showing only the functionality by hiding the implementation
+ * 2. To achieve 0 to 100% abstraction we create abstract class
+ * 3. We can't instantiate an abstract class as we have undefined methods
+ * 4. We can use extends keyword for an abstract class
+ * 5. Abstract class can have both abstract as well as non abstract methods
+ * 6. We can extend only one abstract class at a time
+ * 7. Abstract can be defined with abstract keyword
+ */
 public abstract class Television {
 
 	int volume;

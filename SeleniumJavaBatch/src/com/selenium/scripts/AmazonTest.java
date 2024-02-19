9c3e505a5b4e17fda686f38a44aacf62c830829f
nareshbabumandula <naresh223@gmail.com>
2024-02-19 20:25:56 +0530

    19-Feb-2024: Naresh: Page Object Model in Selenium

--- SeleniumJavaBatch/src/com/selenium/scripts/AmazonTest.java
+++ SeleniumJavaBatch/src/com/selenium/scripts/AmazonTest.java
@@ -1,5 +1,6 @@
 package com.selenium.scripts;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -7,16 +8,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.utilties.Utility;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AmazonTest {
 
 	static WebDriver driver;
-	
-	public static void main(String[] args) throws InterruptedException {
+		
+	public static void main(String[] args) throws InterruptedException, IOException {
 		WebDriverManager.chromedriver().setup();
+		
 		driver = new ChromeDriver();
-		driver.get("https://www.amazon.in/");
+		driver.get(Utility.readProperty("url"));
 		driver.manage().window().maximize();
 		Thread.sleep(20000);
 		List<WebElement> menus = driver.findElements(By.xpath("//div[@id='nav-xshop']/child::a"));

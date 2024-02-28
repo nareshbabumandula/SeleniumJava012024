393e9e9786aa761e71feb551c3d8e61823005aac
nareshbabumandula <naresh223@gmail.com>
2024-02-28 20:29:59 +0530

    Checking in BDD Cucumber project

---
+++ EGW_BDDCucumber/src/test/java/com/runner/Runner.java
@@ -0,0 +1,13 @@
+package com.runner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features", glue="step.definitions",
+tags="@smoke", plugin= {"pretty","html:target/cucumber-reports.html"})
+public class Runner {
+	
+	
+}

4d95b1383f5d491ee55e46091227038a5771167e
nareshbabumandula <naresh223@gmail.com>
2024-03-05 20:10:42 +0530

    05-Mar-2024: Naresh: Scenario, Scenario Outline and Databtable in Feature file

--- EGW_BDDCucumber/src/main/java/step/definitions/StoreSearchPageDefinitions.java
+++ EGW_BDDCucumber/src/main/java/step/definitions/StoreSearchPageDefinitions.java
@@ -1,6 +1,8 @@
 package step.definitions;
 
 import java.time.Duration;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import com.main.Base;
 import com.relevantcodes.extentreports.LogStatus;
@@ -10,7 +12,7 @@ import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class StoreSearchPageDefinitions {
-	
+
 
 	@Given("I access eyeglassworld website")
 	public void accessSite() {
@@ -18,28 +20,57 @@ public class StoreSearchPageDefinitions {
 		Base.getDriver().get("https://www.eyeglassworld.com");
 		Base.getDriver().manage().window().maximize();
 		Base.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-	    System.out.println("Accessed eye glass world website");
+		System.out.println("Accessed eye glass world website");
 	}
-	
+
 	@When("I search for a store based on {string} name")
-	public void i_search_for_a_store_based_on_name(String string) {
+	public void storeSearch(String string) {
 		Base.getDriver().findElement(By.xpath("//input[@placeholder='City, State, or ZIP code']")).sendKeys(string);
 		Base.getDriver().findElement(By.xpath("//button[contains(text(),'Find a Store')]")).click();
-	    System.out.println("Searched an eye glass store based on city/state/zipcode : " + string);
-	    Hooks.getExtentTest().log(LogStatus.PASS, "Searched an eye glass store based on city/state/zipcode : " + string);
+		System.out.println("Searched an eye glass store based on city/state/zipcode : " + string);
+		Hooks.getExtentTest().log(LogStatus.PASS, "Searched an eye glass store based on city/state/zipcode : " + string);
 	}
-	
-	@Then("I should see the appropriate results")
-	public void verifySearchResults() {
-		String actCity = Base.getDriver().findElement(By.xpath("//span[@class='nearyou']")).getText();
-		
-		if (actCity.contains("tampa")) {
-			Hooks.getExtentTest().log(LogStatus.PASS, "City name is found in the search results heading");
-			System.out.println("City name is found in the search results heading");
-		} else {
-			Hooks.getExtentTest().log(LogStatus.FAIL, "ity name is not found in the search results heading");
-			System.out.println("ity name is not found in the search results heading");
+
+	@Then("I should see the appropriate results with city\\/state\\/zipcode {string}")
+	public void verifySearchResults(String cityStateZip) {
+		try {
+			String actCityStateZip = Base.getDriver().findElement(By.xpath("//span[@class='nearyou']")).getText();
+
+			if (actCityStateZip.contains(cityStateZip)) {
+				Hooks.getExtentTest().log(LogStatus.PASS, "City/State/Zipcode name is found in the search results heading");
+				System.out.println("City/State/Zipcode is found in the search results heading");
+			}else {
+				Hooks.getExtentTest().log(LogStatus.FAIL, "City/State/Zipcode name is not found in the search results heading");
+				System.out.println("City/State/Zipcode is not found in the search results heading");
+			}
+		} catch (Exception e) {
+			Hooks.getExtentTest().log(LogStatus.FAIL, "City/State/Zipcode name is not found in the search results heading");
+			System.out.println("City/State/Zipcode is not found in the search results heading");
+			e.printStackTrace();
 		}
+	}
+
+	@When("I search for a store based on city\\/state\\/zipcode")
+	public void i_search_for_a_store_based_on_city_state_zipcode(io.cucumber.datatable.DataTable dataTable) {
+		List<String> data = dataTable.asList();
+
+		for (String citystatezipcode : data) {
+			try {
+				Base.getDriver().findElement(By.xpath("//input[@placeholder='City, State, or ZIP code']")).sendKeys(citystatezipcode);
+				Base.getDriver().findElement(By.xpath("//button[contains(text(),'Find a Store')]")).click();
+				System.out.println("Searched an eye glass store based on city/state/zipcode : " + citystatezipcode);
+				Hooks.getExtentTest().log(LogStatus.PASS, "Searched an eye glass store based on city/state/zipcode : " + citystatezipcode);
+			} catch (Exception e) {
+				Hooks.getExtentTest().log(LogStatus.FAIL, "Failed to enter City/State/Zipcode in the search field");
+				System.out.println("Failed to enter City/State/Zipcode in the search field");
+				e.printStackTrace();
+			}finally {
+				Base.getDriver().navigate().back(); // Browser navigate backward
+				Base.getDriver().navigate().refresh(); // Refresh the browser
+			}
+
+		}
+
 	}
 
 

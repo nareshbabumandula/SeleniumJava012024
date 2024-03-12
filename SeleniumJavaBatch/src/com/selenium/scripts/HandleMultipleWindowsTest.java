a10e5111188af679c5d7e437bfdaccfa31834e6d
nareshbabumandula <naresh223@gmail.com>
2024-03-12 07:59:11 +0530

    12-Mar-2024: Naresh: Working with multiple windows and broken links in Selenium

--- SeleniumJavaBatch/src/com/selenium/scripts/HandleMultipleWindowsTest.java
+++ SeleniumJavaBatch/src/com/selenium/scripts/HandleMultipleWindowsTest.java
@@ -1,13 +1,14 @@
 package com.selenium.scripts;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.safari.SafariDriver.WindowType;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,21 +18,40 @@ public class HandleMultipleWindowsTest {
 	
 	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(); // first window
 		driver.get("https://www.mycontactform.com/");
 		driver.manage().window().maximize();
 		
 		WebElement sampleForms = driver.findElement(By.linkText("Sample Forms"));
 		String s = Keys.chord(Keys.CONTROL, Keys.RETURN);
-		sampleForms.sendKeys(s);
+		sampleForms.sendKeys(s); // second window
 		WebElement features = driver.findElement(By.linkText("Features"));
-		sampleForms.sendKeys(s);
+		sampleForms.sendKeys(s); // third window
+		//driver.switchTo().newWindow(WindowType.TAB); // fourth window
 		String mainWindow = driver.getWindowHandle();
 		Set<String> windows = driver.getWindowHandles();
 		System.out.println(mainWindow);
 		System.out.println(windows);
+		
+		Iterator<String> iter = windows.iterator();
+		
+		while (iter.hasNext()) {
+			String childWindow = iter.next();
+			driver.switchTo().window(childWindow);
+			String title = driver.getTitle();
+			System.out.println(childWindow);
+			if (title.contains("Sample Email Forms")) {
+				driver.findElement(By.id("subject")).sendKeys("Test Subject");
+				Thread.sleep(3000);
+				driver.close();
+			}
+
+		}
+		driver.switchTo().window(mainWindow);
+		driver.findElement(By.name("user")).sendKeys("Praveen");
 		Thread.sleep(3000);
-		driver.quit();
+		
+		driver.quit(); // Closes all the windows opened by webdriver. WebDriver browser session will be terminated completely
 
 	}
 

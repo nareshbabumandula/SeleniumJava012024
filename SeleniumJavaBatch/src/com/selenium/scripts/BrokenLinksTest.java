aafc67cf584e2b72456e44865a8262afdcddb8b9
nareshbabumandula <naresh223@gmail.com>
2024-03-18 07:45:42 +0530

    18-Mar-2024: Naresh: Working with Select class in Selenium

--- SeleniumJavaBatch/src/com/selenium/scripts/BrokenLinksTest.java
+++ SeleniumJavaBatch/src/com/selenium/scripts/BrokenLinksTest.java
@@ -1,5 +1,8 @@
 package com.selenium.scripts;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.time.Duration;
 import java.util.List;
 
@@ -14,21 +17,35 @@ public class BrokenLinksTest {
 
 	static WebDriver driver;
 	
+	public static void verifyLink(String url) {
+		try { 
+			URL link = new URL(url);
+			HttpURLConnection httpURLConnection = (HttpURLConnection)link.openConnection();
+			httpURLConnection.setConnectTimeout(3000);
+			httpURLConnection.connect();
+			if(httpURLConnection.getResponseCode()==200) {
+				System.out.println(url + "-" + httpURLConnection.getResponseMessage());
+			}else {
+				System.out.println(url + "-" + httpURLConnection.getResponseMessage() + "-" + "is a broken link");
+			}
+	
+		} catch (Exception e) {
+			System.out.println(url + "-" + "is a broken link");
+		}
+	}
+	
 	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver(); // first window
-		driver.get("https://demo.guru99.com/test/newtours/register.php");
+		driver = new ChromeDriver();
+		driver.get("https://www.advantageonlineshopping.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		Thread.sleep(5000);
 		List<WebElement> links = driver.findElements(By.tagName("a"));
-		
+		System.out.println("No of links found are : " + links.size());
 		for(WebElement link : links) {
-			if (link.getAttribute("href")!=null) {
-				System.out.println(link.getText());
-			} else {
-				System.out.println(link.getText() + " is a broken link");
-			}
+			String url = link.getAttribute("href");
+			verifyLink(url);
 		}
 		
 		driver.quit();

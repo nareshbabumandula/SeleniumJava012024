60232e78aa8a991e07c52307fdb49da34d23964b
nareshbabumandula <naresh223@gmail.com>
2024-03-25 07:44:48 +0530

    String methods

--- SeleniumJavaBatch/src/com/java/basics/StringMethods.java
+++ SeleniumJavaBatch/src/com/java/basics/StringMethods.java
@@ -33,6 +33,31 @@ public class StringMethods {
 		String strText = "                  welcome to java training              ";
 		System.out.println(strText);
 		System.out.println(strText.trim());
+		
+		System.out.println(sText.charAt(0));
+		System.out.println(sText.charAt(1));
+		//System.out.println(sText.charAt(9)); // StringIndexOutOfBoundsException
+		
+		System.out.println("Retrieve each character from a string..!");
+		for (int i = 0; i<=sText.length()-1; i++) {
+			System.out.println(sText.charAt(i));
+		}
+		
+		System.out.println("Retrieve each character from a string in a reverse order..!");
+		System.out.println("Reverse a string..!");
+		String rev="";
+		for (int i = sText.length()-1; i>=0; i--) {
+			rev+=sText.charAt(i);
+		}
+		System.out.println(rev);
+		
+		String str1 = "welcome to java sofware edition";
+		System.out.println(str1.contains("java"));
+		System.out.println(str1.contains("    JAVA    ".toLowerCase().trim()));
+		
+		System.out.println(str1.replace("java", "core java"));
+		System.out.println(str1.concat(" course"));
+		
 	}
 
 }
